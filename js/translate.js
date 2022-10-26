60ccf18782ff97c31325c8eee5a0082f31b5e0bd
sheyda99 <sheydasultan.99@gmail.com>
2022-10-27 01:49:48 +0400

    changing api

--- js/translate.js
+++ js/translate.js
@@ -1,5 +1,5 @@
 function Translate(word, language) {
-    this.apikey = 'a6a351e606mshc5ff9e70ecdaba2p1cb404jsn059b16ddfb0b';
+    this.apikey = '98a4e8797dmsh3f09630434206e0p1d3c8djsn2bb30271e879';
     this.word = word;
     this.language = language;
 
@@ -7,17 +7,17 @@ function Translate(word, language) {
 }
 
 Translate.prototype.translateWord = function(callback) {
-    const endpoint = `https://just-translated.p.rapidapi.com/?lang=az-${this.language}&text=${this.word}`;
+    const endpoint = `https://nlp-translation.p.rapidapi.com/v1/translate?text=${this.word}&to=${this.language}&from=en`;
     const data = null;
 
     this.xhr.open("GET", endpoint);
-    this.xhr.setRequestHeader("X-RapidAPI-Host", "just-translated.p.rapidapi.com");
     this.xhr.setRequestHeader("X-RapidAPI-Key", this.apikey);
+    this.xhr.setRequestHeader("X-RapidAPI-Host", "nlp-translation.p.rapidapi.com");
 
     this.xhr.onload = () => {
         if (this.xhr.status === 200) {
             const json = JSON.parse(this.xhr.responseText);
-            const text = json.text[0];
+            const text = json.translated_text[this.language];
             callback(null,text);
         } else {
             callback("Error!",null);
